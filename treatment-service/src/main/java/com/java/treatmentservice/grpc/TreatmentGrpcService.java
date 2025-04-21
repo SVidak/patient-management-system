92c0e190fd70545c6127f4c4917aef7a63e9043f
SVidak <svidakofficial@gmail.com>
2025-04-21 13:14:09 +0200

    treatment-service grpc getAll, update, delete crud

--- treatment-service/src/main/java/com/java/treatmentservice/grpc/TreatmentGrpcService.java
+++ treatment-service/src/main/java/com/java/treatmentservice/grpc/TreatmentGrpcService.java
@@ -8,6 +8,9 @@ import net.devh.boot.grpc.server.service.GrpcService;
 import treatment.CreateTreatmentResponse;
 import treatment.TreatmentServiceGrpc;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @GrpcService
 public class TreatmentGrpcService extends TreatmentServiceGrpc.TreatmentServiceImplBase {
 
@@ -39,6 +42,26 @@ public class TreatmentGrpcService extends TreatmentServiceGrpc.TreatmentServiceI
     }
 
     @Override
+    public void getAllTreatments(treatment.GetAllTreatmentsRequest getAllTreatmentsRequest, StreamObserver<treatment.GetAllTreatmentsResponse> responseObserver) {
+
+        List<TreatmentResponseDTO> allTreatments = treatmentService.getTreatments();
+
+        List<treatment.Treatment> grpcTreatments = allTreatments.stream().map(dto -> treatment.Treatment.newBuilder()
+                .setId(dto.getId())
+                .setName(dto.getName())
+                .setDescription(dto.getDescription())
+                .setPrice(dto.getPrice())
+                .setDuration(dto.getDuration())
+                .build()).collect(Collectors.toList());
+
+        treatment.GetAllTreatmentsResponse getAllTreatmentsResponse = treatment.GetAllTreatmentsResponse.newBuilder()
+                .addAllTreatments(grpcTreatments).build();
+
+        responseObserver.onNext(getAllTreatmentsResponse);
+        responseObserver.onCompleted();
+    }
+
+    @Override
     public void createTreatment(treatment.CreateTreatmentRequest createTreatmentRequest, StreamObserver<CreateTreatmentResponse> responseObserver) {
 
         TreatmentRequestDTO treatmentRequestDTO = new TreatmentRequestDTO();
@@ -64,4 +87,40 @@ public class TreatmentGrpcService extends TreatmentServiceGrpc.TreatmentServiceI
         responseObserver.onCompleted();
 
     }
+
+    @Override
+    public void updateTreatment(treatment.UpdateTreatmentRequest updateTreatmentRequest, StreamObserver<treatment.UpdateTreatmentResponse> responseObserver) {
+
+        TreatmentRequestDTO treatmentRequestDTO = new TreatmentRequestDTO();
+        treatmentRequestDTO.setName(updateTreatmentRequest.getName());
+        treatmentRequestDTO.setDescription(updateTreatmentRequest.getDescription());
+        treatmentRequestDTO.setPrice(updateTreatmentRequest.getPrice());
+        treatmentRequestDTO.setDuration(updateTreatmentRequest.getDuration());
+
+        TreatmentResponseDTO treatmentResponseDTO = treatmentService.updateTreatment(updateTreatmentRequest.getId(), treatmentRequestDTO);
+
+        treatment.Treatment grpcTreatment = treatment.Treatment.newBuilder()
+                .setId(treatmentResponseDTO.getId())
+                .setName(treatmentResponseDTO.getName())
+                .setDescription(treatmentResponseDTO.getDescription())
+                .setPrice(treatmentResponseDTO.getPrice())
+                .setDuration(treatmentResponseDTO.getDuration())
+                .build();
+
+        treatment.UpdateTreatmentResponse updateTreatmentResponse = treatment.UpdateTreatmentResponse.newBuilder()
+                .setTreatment(grpcTreatment).build();
+
+        responseObserver.onNext(updateTreatmentResponse);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void deleteTreatment(treatment.DeleteTreatmentRequest deleteTreatmentRequest, StreamObserver<treatment.DeleteTreatmentResponse> responseObserver) {
+
+        treatmentService.deleteTreatment(deleteTreatmentRequest.getId());
+
+        responseObserver.onNext(treatment.DeleteTreatmentResponse.newBuilder().build());
+        responseObserver.onCompleted();
+    }
+
 }
